2853cfcd7e22bfe78440f11c35596736ff47486c
skygodbas <2238178090@qq.com>
2017-09-21 10:23:16 +0800

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,19 @@
 /**
- * Created by Administrator on 2017/8/28.
+ * Created by Administrator on 2017/9/19.
  */
+$(function(){
+    //setInterval(function(){
+    //    ChangeImg();
+    //},1000);
+    function ChangeImg(){
+        $("header img[class='selected']").removeClass("selected").siblings("img").addClass("selected");
+    };
+
+
+    //光标闪烁修改内容：
+    if( $("#introduce")[0].style.width == 0 ){
+        $("#introduce").text("php developer").animate({
+            width:'62%'
+        },1000);
+    }
+});
