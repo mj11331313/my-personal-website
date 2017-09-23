5ab19a99939e729bc5aa4b8b0cd0b85890d788c5
mj11331313 <2238178090@qq.com>
2017-09-23 11:21:55 +0800

    update

--- javascript/index.js
+++ javascript/index.js
@@ -13,5 +13,16 @@ $(function(){
     //光标闪烁修改内容：
     if( $("#introduce")[0].offsetWidth == 0 ){
         console.log("kong")
-    }
+    };
+
+
+    //top菜单点击切换样式：
+    $("#header-mask li").on("click",function(){
+        if( $(this).hasClass("active") ){
+            $(this).css("color","#337ab7");
+        }else{
+            $(this).css("color","#fff");
+        }
+        $(this).addClass("active").siblings().removeClass("active");
+    });
 });
