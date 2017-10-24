be7e92684615349e09effd7e27cd7716559037b8
mj11331313 <2238178090@qq.com>
2017-10-24 16:45:57 +0800

    主页

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,17 @@
 /**
- * Created by Administrator on 2017/8/28.
+ * Created by Administrator on 2017/9/19.
  */
+$(function(){
+    //setInterval(function(){
+    //    ChangeImg();
+    //},1000);
+    function ChangeImg(){
+        $("header img[class='selected']").removeClass("selected").siblings("img").addClass("selected");
+    };
+
+
+    //光标闪烁修改内容：
+    if( $("#introduce")[0].offsetWidth == 0 ){
+        console.log("kong")
+    }
+});
